91827c7b8bef47c151ba013c38385657b4ba1096
Ryan Ong <ryanong@gmail.com>
2013-02-03 01:05:37 -0500

    remove usage of instance variables where possible

--- lib/spies/spy.rb
+++ lib/spies/spy.rb
@@ -13,28 +13,27 @@ module Spies
       opts[:force] ||= false
       if method_visibility || !opts[:force]
         @original_method = base_object.method(method_name)
-        @arity_range = get_arity_range(@original_method.parameters)
         if base_object.singleton_methods.include?(method_name)
-          @base_object.singleton_class.send(:remove_method, method_name)
+          base_object.singleton_class.send(:remove_method, method_name)
         end
       end
 
       __spies_spy = self
-      @base_object.define_singleton_method(method_name) do |*args, &block|
+      base_object.define_singleton_method(method_name) do |*args, &block|
         __spies_spy.record(self,args,block)
       end
 
       opts[:visibility] ||= method_visibility
-      @base_object.singleton_class.send(opts[:visibility], method_name) if opts[:visibility]
+      base_object.singleton_class.send(opts[:visibility], method_name) if opts[:visibility]
       @hooked = true
       self
     end
 
     def unhook
       raise "#{method_name} method has not been hooked" unless hooked?
-      @base_object.singleton_class.send(:remove_method, method_name)
-      @base_object.define_singleton_method(method_name, @original_method) if @original_method
-      @base_object.singleton_class.send(method_visibility, method_name) if method_visiblity
+      base_object.singleton_class.send(:remove_method, method_name)
+      base_object.define_singleton_method(method_name, original_method) if original_method
+      base_object.singleton_class.send(method_visibility, method_name) if method_visiblity
       clear_method!
       self
     end
@@ -54,18 +53,18 @@ module Spies
     end
 
     def called?
-      @calls.size > 0
+      calls.size > 0
     end
 
     def called_with?(*args)
-      @calls.any? do |call_log|
+      calls.any? do |call_log|
         call_log.args == args
       end
     end
 
     def record(object, args, block)
       check_arity!(args.size)
-      @calls << CallLog.new(object, args, block)
+      calls << CallLog.new(object, args, block)
       @plan.call(*args, &block) if @plan
     end
 
@@ -96,28 +95,33 @@ module Spies
     end
 
     def check_arity!(arity)
-      return unless @arity_range
-      if arity < @arity_range.min
-        raise ArgumentError.new("wrong number of arguments (#{arity} for #{@arity_range.min})")
-      elsif arity > @arity_range.max
-        raise ArgumentError.new("wrong number of arguments (#{arity} for #{@arity_range.max})")
+      return unless arity_range
+      if arity < arity_range.min
+        raise ArgumentError.new("wrong number of arguments (#{arity} for #{arity_range.min})")
+      elsif arity > arity_range.max
+        raise ArgumentError.new("wrong number of arguments (#{arity} for #{arity_range.max})")
       end
     end
 
-    def get_arity_range(parameters)
-      min = max = 0
-      parameters.each do |type,_|
-        case type
-        when :req
-          min += 1
-          max += 1
-        when :opt
-          max += 1
-        when :rest
-          max = Float::INFINITY
-        end
+    def arity_range
+      if original_method
+        @arity_range ||=
+          begin
+            min = max = 0
+            original_method.parameters.each do |type,_|
+              case type
+              when :req
+                min += 1
+                max += 1
+              when :opt
+                max += 1
+              when :rest
+                max = Float::INFINITY
+              end
+            end
+            (min..max)
+          end
       end
-      (min..max)
     end
 
     class << self
