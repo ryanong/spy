b839913bc50b9e07e70e67f76608d4713bbfaf7b
Ryan Ong <ryanong@gmail.com>
2013-02-10 17:56:12 -0500

    fix stub spec

--- spec/spy/stub_spec.rb
+++ spec/spy/stub_spec.rb
@@ -29,31 +29,31 @@ module RSpec
         @stub = Object.new
       end
 
-      describe "using Spy.stub" do
+      describe "using Spy.new" do
         it "returns declared value when message is received" do
-          Spy.stub(@instance, :msg).and_return(:return_value)
+          Spy.new(@instance, :msg).hook(force: true).and_return(:return_value)
           expect(@instance.msg).to equal(:return_value)
         end
       end
 
       it "instructs an instance to respond_to the message" do
-        Spy.stub(@instance, :msg)
+        Spy.new(@instance, :msg).hook(force: true)
         expect(@instance).to respond_to(:msg)
       end
 
       it "instructs a class object to respond_to the message" do
-        Spy.stub(@class, :msg)
+        Spy.new(@class, :msg).hook(force: true)
         expect(@class).to respond_to(:msg)
       end
 
       it "handles multiple stubbed methods" do
-        Spy.stub(@instance, :msg1 => 1, :msg2 => 2)
+        Spy.new(@instance, :msg1 => 1, :msg2 => 2).hook(force: true)
         expect(@instance.msg1).to eq(1)
         expect(@instance.msg2).to eq(2)
       end
 
       it "yields a specified object" do
-        Spy.stub(@instance, :method_that_yields).and_yield(:yielded_obj)
+        Spy.new(@instance, :method_that_yields).hook(force: true).and_yield(:yielded_obj)
         current_value = :value_before
         @instance.method_that_yields {|val| current_value = val}
         expect(current_value).to eq :yielded_obj
@@ -61,23 +61,23 @@ module RSpec
 
       it "yields a specified object and return another specified object" do
         yielded_obj = double("my mock")
-        Spy.stub(yielded_obj, :foo)
-        Spy.stub(@instance, :method_that_yields_and_returns).and_yield(yielded_obj).and_return(:baz)
+        Spy.new(yielded_obj, :foo).hook(force: true)
+        Spy.new(@instance, :method_that_yields_and_returns).hook(force: true).and_yield(yielded_obj).and_return(:baz)
         expect(@instance.method_that_yields_and_returns { |o| o.foo :bar }).to eq :baz
       end
 
       it "throws when told to" do
-        Spy.stub(@stub, :something).and_throw(:up)
+        Spy.new(@stub, :something).hook(force: true).and_throw(:up)
         expect { @stub.something }.to throw_symbol(:up)
       end
 
       it "throws with argument when told to" do
-        Spy.stub(@stub, :something).and_throw(:up, 'high')
+        Spy.new(@stub, :something).hook(force: true).and_throw(:up, 'high')
         expect { @stub.something }.to throw_symbol(:up, 'high')
       end
 
       it "overrides a pre-existing method" do
-        Spy.stub(@stub, :existing_instance_method).and_return(:updated_stub_value)
+        Spy.new(@stub, :existing_instance_method).hook(force: true).and_return(:updated_stub_value)
         expect(@stub.existing_instance_method).to eq :updated_stub_value
       end
     end
