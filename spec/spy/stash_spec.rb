f8b1662b490e8099872ab9b74875e5621989c887
Ryan Ong <ryanong@gmail.com>
2013-02-22 10:37:09 -0500

    remove double test and fix stash_spec

--- spec/spy/stash_spec.rb
+++ spec/spy/stash_spec.rb
@@ -1,54 +1,47 @@
 require 'spec_helper'
 
-module RSpec
-  module Mocks
-
-    describe "only stashing the original method" do
-      let(:klass) do
-        Class.new do
-          def self.foo(arg)
-            :original_value
-          end
+module Spy
+  describe "only stashing the original method" do
+    let(:klass) do
+      Class.new do
+        def self.foo(arg)
+          :original_value
         end
-      end
-
-      it "keeps the original method intact after multiple expectations are added on the same method" do
-        klass.should_receive(:foo).with(:fizbaz).and_return(:wowwow)
-        klass.should_receive(:foo).with(:bazbar).and_return(:okay)
-
-        klass.foo(:fizbaz)
-        klass.foo(:bazbar)
-        klass.rspec_verify
-
-        klass.rspec_reset
-        expect(klass.foo(:yeah)).to equal(:original_value)
       end
     end
 
-    describe "when a class method is aliased on a subclass and the method is mocked" do
-      let(:klass) do
-        Class.new do
-          class << self
-            alias alternate_new new
-          end
+    it "keeps the original method intact after multiple expectations are added on the same method" do
+      spy = Spy.on(klass, :foo)
+      klass.foo(:bazbar)
+      expect(spy).to have_been_called
+      Spy.off(klass, :foo)
+
+      expect(klass.foo(:yeah)).to equal(:original_value)
+    end
+  end
+
+  describe "when a class method is aliased on a subclass and the method is mocked" do
+    let(:klass) do
+      Class.new do
+        class << self
+          alias alternate_new new
+        end
+      end
+    end
+
+    it "restores the original aliased public method" do
+      klass = Class.new do
+        class << self
+          alias alternate_new new
         end
       end
 
-      it "restores the original aliased public method" do
-        klass = Class.new do
-          class << self
-            alias alternate_new new
-          end
-        end
+      spy = Spy.on(klass, :alternate_new)
+      expect(klass.alternate_new).to be_nil
+      expect(spy).to have_been_called
 
-        klass.should_receive(:alternate_new)
-        expect(klass.alternate_new).to be_nil
-
-        klass.rspec_verify
-
-        klass.rspec_reset
-        expect(klass.alternate_new).to be_an_instance_of(klass)
-      end
+      Spy.off(klass, :alternate_new)
+      expect(klass.alternate_new).to be_an_instance_of(klass)
     end
   end
 end
