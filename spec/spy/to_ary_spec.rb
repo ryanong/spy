48c88804d619a25f1c9a1393a88df4ad3e1a5986
Ryan Ong <ryanong@gmail.com>
2013-02-22 10:31:27 -0500

    remove test_double_spec and make half of to_ary_spec pass

--- spec/spy/to_ary_spec.rb
+++ spec/spy/to_ary_spec.rb
@@ -13,28 +13,28 @@ describe "a double receiving to_ary" do
     end
 
     it "can be overridden with a stub" do
-      Spy.on(obj, :to_ary) { :non_nil_value }
+      Spy::Subroutine.new(obj, :to_ary).hook(force: true).and_return(:non_nil_value)
       expect(obj.to_ary).to be(:non_nil_value)
     end
 
     it "responds when overriden" do
-      Spy.on(obj, :to_ary) { :non_nil_value }
+      Spy::Subroutine.new(obj, :to_ary).hook(force: true).and_return(:non_nil_value)
       expect(obj).to respond_to(:to_ary)
     end
 
     it "supports Array#flatten" do
-      obj = double('foo')
+      obj = Spy.double('foo')
       expect([obj].flatten).to eq([obj])
     end
   end
 
   context "double as_null_object" do
-    let(:obj) { double('obj').as_null_object }
+    let(:obj) { Spy.double('obj').as_null_object }
     include_examples "to_ary"
   end
 
   context "double without as_null_object" do
-    let(:obj) { double('obj') }
+    let(:obj) { Spy.double('obj') }
     include_examples "to_ary"
   end
 end
