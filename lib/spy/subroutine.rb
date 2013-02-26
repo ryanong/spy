17c65e3a15ba3f20bfb4b4aca674ebf436266562
Ryan Ong <ryanong@gmail.com>
2013-02-26 11:03:06 -0500

    simplify Subroutine.get_spies

--- lib/spy/subroutine.rb
+++ lib/spy/subroutine.rb
@@ -305,14 +305,16 @@ module Spy
       # @param singleton_method [Boolean] (true) only get singleton_method_spies
       # @return [Array<Subroutine>]
       def get_spies(base_object, singleton_methods = true)
-        if singleton_methods
-        all_methods = base_object.public_methods(false) +
-          base_object.protected_methods(false) +
-          base_object.private_methods(false)
-        else
-          all_methods = base_object.instance_methods(false) +
-            base_object.private_instance_methods(false)
-        end
+        all_methods =
+          if singleton_methods
+            base_object.public_methods(false) +
+              base_object.protected_methods(false) +
+              base_object.private_methods(false)
+          else
+            base_object.public_instance_methods(false) +
+              base_object.protected_instance_methods(false) +
+              base_object.private_instance_methods(false)
+          end
 
         all_methods.map do |method_name|
           Agency.instance.find(get_spy_id(base_object.method(method_name)))
