55bc7b0edc5f43afb1ee093195d334862ca7f289
Ryan Ong <ryanong@gmail.com>
2013-03-10 22:46:05 -0300

    fix exceptions so you can add custom errors

--- lib/spy/exceptions.rb
+++ lib/spy/exceptions.rb
@@ -2,32 +2,32 @@ module Spy
   class Error < StandardError; end
 
   class AlreadyStubbedError < Error
-    def message
-      "Spy is already stubbed."
+    def to_s
+      @mesg || "Spy is already stubbed."
     end
   end
 
   class AlreadyHookedError < Error
-    def message
-      "Spy is already hooked."
+    def to_s
+      @mesg || "Spy is already hooked."
     end
   end
 
   class NotHookedError < Error
-    def message
-      "Spy was not hooked."
+    def to_s
+      @mesg || "Spy was not hooked."
     end
   end
 
   class NeverHookedError < Error
-    def message
-      "Spy was never hooked."
+    def to_s
+      @mesg || "Spy was never hooked."
     end
   end
 
   class NoSpyError < Error
-    def message
-      "Spy could not be found"
+    def to_s
+      @mesg || "Spy could not be found"
     end
   end
 end
